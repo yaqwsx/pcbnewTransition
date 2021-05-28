ea643a957f520a01c2cb703c2c7685f3b22bc9d6
Jan Mrázek <email@honzamrazek.cz>
2021-05-28 16:54:36 +0200

    Improve compatibility

--- pcbnewTransition/transition.py
+++ pcbnewTransition/transition.py
@@ -1,5 +1,6 @@
 
 import pcbnew
+import types
 
 # KiCAD 6 renames some of the types, ensure compatibility by introducing aliases
 # when KiCAD 5 is used
@@ -18,12 +19,19 @@ def boardGetProperties(self):
 def boardSetProperties(self, p):
     raise NotImplementedError("The API is gone in v6, should be back in a few months")
 
-def isV6(version):
+def GetBoundingBox(self, includeText=True, includeInvisibleText=True):
+    if not includeText and not includeInvisibleText:
+        return self.GetFootprintRect()
+    if includeText and includeInvisibleText:
+        return self._GetBoundingBox()
+    raise NotImplementedError("Incompatible v5 and v6 API")
+
+pcbnewVersion = getVersion()
+
+def isV6(version=pcbnewVersion):
     if version[0] == 5 and version[1] == 99:
         return True
     return version[0] == 6
-
-pcbnewVersion = getVersion()
 
 if not isV6(pcbnewVersion):
     # Introduce type aliases
@@ -39,7 +47,18 @@ if not isV6(pcbnewVersion):
 
     # Introduce renamed functions
     pcbnew.BOARD.GetFootprints = pcbnew.BOARD.GetModules
+    pcbnew.BOARD.FindFootprintByReference = pcbnew.BOARD.FindModuleByReference
+
+    pcbnew.MODULE._GetBoundingBox = pcbnew.MODULE.GetBoundingBox
+    pcbnew.MODULE.GetBoundingBox = GetBoundingBox
 
     # Add board properties
     pcbnew.BOARD.GetProperties = boardGetProperties
     pcbnew.BOARD.SetProperties = boardSetProperties
+
+    # PLOTTING ENUMS
+    pcbnew.PLOT_TEXT_MODE_STROKE = pcbnew.PLOTTEXTMODE_STROKE
+    pcbnew.PLOT_TEXT_MODE_DEFAULT = pcbnew.PLOTTEXTMODE_DEFAULT
+    pcbnew.PLOT_TEXT_MODE_NATIVE = pcbnew.PLOTTEXTMODE_NATIVE
+    pcbnew.PLOT_TEXT_MODE_PHANTOM = pcbnew.PLOTTEXTMODE_PHANTOM
+
