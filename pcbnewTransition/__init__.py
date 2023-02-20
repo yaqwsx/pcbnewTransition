847cbbbae34433f8c007c5591d2b5de1ec92ed3b
Jan Mrázek <email@honzamrazek.cz>
2023-02-20 08:58:15 +0100

    Introduce Debian/Ubuntu loading hack for v7

--- pcbnewTransition/__init__.py
+++ pcbnewTransition/__init__.py
@@ -3,4 +3,21 @@ from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
+# A (temporary) hack follows!
+#
+# Some packages on Linux for v7 change the location of the pcbnew module, let's
+# add the new location to path. The native library is also in this location,
+# thus, the loader can't load it. We trick it into loading by loading it first
+# by an absolute path
+import os
+import sys
+if os.name != "nt":
+    sys.path.append("/usr/lib/kicad/lib/python3/dist-packages")
+    try:
+        from ctypes import cdll
+        cdll.LoadLibrary("/usr/lib/kicad/lib/x86_64-linux-gnu/libkicad_3dsg.so.2.0.0")
+    except Exception:
+        pass # Ignore any errors as the library just might not exists here
+
+
 from .transition import *
