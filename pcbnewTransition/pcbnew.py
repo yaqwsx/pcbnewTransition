8819875536c089d812eaf2eb398dbd7e46d7f57a
Jan Mrázek <email@honzamrazek.cz>
2023-05-30 15:08:29 +0200

    Revert "Use inheritance approach to preserve compatibility with other plugins"
    This reverts commit ee52de4e73b22a5eca93b8c97e52b111dc267911.

--- pcbnewTransition/pcbnew.py
+++ pcbnewTransition/pcbnew.py
@@ -1,12 +1,1 @@
-import pcbnew as pcbnewOrig
 from pcbnew import *
-import inspect
-
-def inherit_classes(module):
-    classes = inspect.getmembers(module, inspect.isclass)
-
-    for class_name, class_obj in classes:
-        new_class = type(class_name, (class_obj,), {})
-        globals()[class_name] = new_class
-
-inherit_classes(pcbnewOrig)
